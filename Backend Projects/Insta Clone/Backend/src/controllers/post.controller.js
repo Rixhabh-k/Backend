11cf4162cd69e4b6c8d88dada22f48226b52af40
rixhabh-k <kharwarrishabh891@gmail.com>
2026-07-22 13:45:15 +0530

    saving images to cloud

--- Backend Projects/Insta Clone/Backend/src/controllers/post.controller.js
+++ Backend Projects/Insta Clone/Backend/src/controllers/post.controller.js
@@ -1,56 +1,51 @@
-const postModel = require("../models/post.model")
-const ImageKit = require("@imagekit/nodejs")
-const {toFile} =  require("@imagekit/nodejs")
-const jwt = require("jsonwebtoken")
+const postModel = require("../models/post.model");
+const ImageKit = require("@imagekit/nodejs");
+const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
 
 const imagekit = new ImageKit({
-     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
-})
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+});
 
+const createPostController = async (req, res) => {
+  const token = req.cookies.token;
 
-const createPostController = async (req,res)=>{
+  if (!token) {
+    return res.status(401).json({
+      message: "token not found",
+    });
+  }
 
-    const token = req.cookies.token
+  let decode = null;
 
-    
+  try {
+    decode = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: " user is unauthorized",
+    });
+  }
 
-    if(!token){
-        return res.status(401).json({
-            message: "token not found"
-        })
-    }
-
-    let decode = null
-
-    try {
-        decode = jwt.verify(token,process.env.JWT_SECRET)
-    } catch (error) {
-        return res.status(401).json({
-            message: " user is unauthorized"
-        })
-    }
-
-   const file = await imagekit.files.upload({
-    file: await toFile(Buffer.from(req.file.buffer),'file'),
+  const file = await imagekit.files.upload({
+    file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "test",
-    folder: "/InstaClone-posts"
-   })
+    folder: "/InstaClone-posts",
+  });
 
-   
-
-   const post = await postModel.create({
+  const post = await postModel.create({
     caption: req.body.caption,
     imgUrl: file.url,
-    user:decode.id
-   })
+    user: decode.id,
+  });
 
-   res.status(201).json({
-    message: "post created successully",file
-   })
-}
+  res.status(201).json({
+    message: "post created successully",
+    file,
+  });
+};
 
 module.exports = {
-    createPostController
-} 
+  createPostController,
+};
